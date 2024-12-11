c1b6a41f6bd5d04963d5ee233f02eeaeace9fd49
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-12-11 12:57:20 +0500

    26. Remove Duplicates from Sorted Array - Two Pointers|Python

---
+++ Easy/Array/26.Remove-Duplicates-from-Sorted-Array.py
@@ -0,0 +1,76 @@
+"""
+26. Remove Duplicates from Sorted Array
+
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+    Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+    Return k.
+
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+
+If all assertions pass, then your solution will be accepted.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 3 * 104
+    -100 <= nums[i] <= 100
+    nums is sorted in non-decreasing order.
+
+    Topics
+Array
+Two Pointers
+
+"""
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        # Pointer to track the position of the last unique element
+        write_index = 0
+
+        # Iterate over the array starting from the second element
+        for read_index in range(1, len(nums)):
+            # If current number is different from the last unique number
+            if nums[read_index] != nums[write_index]:
+                write_index += 1  # Move the write pointer forward
+                # Update with the new unique number
+                nums[write_index] = nums[read_index]
+
+        # Return the count of unique elements
+        return write_index + 1
