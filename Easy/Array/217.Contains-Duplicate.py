145565e29858219a85b3c60548f9ecc59a22fb2c
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-12-22 09:43:54 +0500

    217. Contains Duplicate - Array|Hash Table|Sorting|Python

---
+++ Easy/Array/217.Contains-Duplicate.py
@@ -0,0 +1,48 @@
+"""
+
+217. Contains Duplicate
+
+
+  Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: true
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+Output: false
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+Output: true
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 105
+    -109 <= nums[i] <= 109
+
+Topics
+Array
+Hash Table
+Sorting
+
+"""
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()  # Set to track unique elements
+        for num in nums:
+            if num in seen:  # Check if num is already in the set
+                return True
+            seen.add(num)  # Add num to the set
+        return False
