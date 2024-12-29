0f4d99a29a508792dd666a85373079675f614746
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2024-12-29 15:39:09 +0500

    350. Intersection of Two Arrays II - Array|Two Pointers|Binary Search|Sorting

--- Easy/Array/350.Intersection-of-Two-Arrays-II.py
+++ Easy/Array/350.Intersection-of-Two-Arrays-II.py
@@ -52,3 +52,26 @@ class Solution:
                 counts[num] -= 1  # Decrement the count
 
         return result
+
+
+# Approach 2: Two Pointers (for Sorted Arrays)
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1.sort()
+        nums2.sort()
+        i, j = 0, 0
+        result = []
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i] == nums2[j]:
+                result.append(nums1[i])
+                i += 1
+                j += 1
+            elif nums1[i] < nums2[j]:
+                i += 1
+            else:
+                j += 1
+
+        return result
