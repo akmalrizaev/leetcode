73a0a333a22304323abd25f092673e990d31f330
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-01-06 09:58:04 +0500

    347. Top K Frequent Elements - Alternative Approach: Bucket Sort

--- Medium/Array/347.Top-K-Frequent-Elements.py
+++ Medium/Array/347.Top-K-Frequent-Elements.py
@@ -69,3 +69,29 @@ class Solution:
 
         # Step 3: Extract the elements from the heap
         return [num for freq, num in heap]
+
+# -------------------------------------------------------------------------------------------------
+
+# Alternative Approach: Bucket Sort
+# Time Complexity: 𝑂(𝑛)
+# Space Complexity:𝑂(𝑛)
+
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Step 1: Count frequencies
+        freq_map = Counter(nums)
+
+        # Step 2: Bucket sort by frequency
+        bucket = [[] for _ in range(len(nums) + 1)]
+        for num, freq in freq_map.items():
+            bucket[freq].append(num)
+
+        # Step 3: Collect top k frequent elements
+        result = []
+        for i in range(len(bucket) - 1, 0, -1):
+            result.extend(bucket[i])
+            if len(result) >= k:
+                break
+
+        return result[:k]
