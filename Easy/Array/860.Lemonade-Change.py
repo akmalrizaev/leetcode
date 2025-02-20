f49859070d78b89e158957fe77b549ff037705c0
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-02-20 10:15:27 +0500

    860. Lemonade Change - Array|Greedy

--- Easy/Array/860.Lemonade-Change.py
+++ Easy/Array/860.Lemonade-Change.py
@@ -38,3 +38,33 @@ Topics
 Array
 Greedy
 """
+from typing import List
+
+
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five, ten = 0, 0
+
+        for bill in bills:
+            if bill == 5:
+                five += 1
+            elif bill == 10:
+                if five == 0:
+                    return False
+                five -= 1
+                ten += 1
+            elif bill == 20:
+                if ten > 0 and five > 0:
+                    ten -= 1
+                    five -= 1
+                elif five >= 3:
+                    five -= 3
+                else:
+                    return False
+        return True
+
+
+# Example usage
+solution = Solution()
+print(solution.lemonadeChange([5, 5, 5, 10, 20]))  # Output: True
+print(solution.lemonadeChange([5, 5, 10, 10, 20]))  # Output: False
