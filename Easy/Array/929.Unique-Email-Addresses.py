03ab795e054bb9109024bafe34d5ac70ce428569
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-03-07 17:04:10 +0500

    929. Unique Email Addresses - Array|Hash Table|String

--- Easy/Array/929.Unique-Email-Addresses.py
+++ Easy/Array/929.Unique-Email-Addresses.py
@@ -43,3 +43,27 @@ Array
 Hash Table
 String
 """
+
+from typing import List
+
+
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        unique_emails = set()
+
+        for email in emails:
+            local, domain = email.split("@")
+            local = local.split("+")[0].replace(".", "")
+            unique_emails.add(local + "@" + domain)
+
+        return len(unique_emails)
+
+
+# Example usage
+solution = Solution()
+print(solution.numUniqueEmails(["test.email+alex@leetcode.com",
+      "test.e.mail+bob.cathy@leetcode.com", "testemail+david@lee.tcode.com"]))
+# Output: 2
+print(solution.numUniqueEmails(
+    ["a@leetcode.com", "b@leetcode.com", "c@leetcode.com"]))
+# Output: 3
