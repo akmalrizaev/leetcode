9e291c8d452789084d94051fd8b49775a28d17f0
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-03-31 16:39:39 +0500

    1002. Find Common Characters - Array|Hash Table|String

--- Easy/Array/1002.Find-Common-Characters.py
+++ Easy/Array/1002.Find-Common-Characters.py
@@ -26,3 +26,35 @@ Array
 Hash Table
 String
 """
+
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        # Initialize the frequency of characters to a large value (inf)
+        common_count = Counter(words[0])
+
+        # Update common_count by taking min frequency with each word
+        for word in words[1:]:
+            word_count = Counter(word)
+            for char in list(common_count.keys()):
+                if char in word_count:
+                    common_count[char] = min(
+                        common_count[char], word_count[char])
+                else:
+                    del common_count[char]
+
+        # Convert the common_count dictionary to a list of characters
+        result = []
+        for char, freq in common_count.items():
+            result.extend([char] * freq)
+
+        return result
+
+
+solution = Solution()
+# Output: ["e","l","l"]
+print(solution.commonChars(["bella", "label", "roller"]))
+print(solution.commonChars(["cool", "lock", "cook"]))     # Output: ["c","o"]
