2cc044614d803b58b64482a5eddb46657db1bac9
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-04-27 16:25:08 +0500

    88. Merge Sorted Array - Array|Two Pointers|Sorting

--- Easy/Array/88.Merge-Sorted-Array.go
+++ Easy/Array/88.Merge-Sorted-Array.go
@@ -53,3 +53,27 @@ Two Pointers
 Sorting
 
 */
+
+package main
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	p1, p2, p := m-1, n-1, m+n-1
+
+	for p1 >= 0 && p2 >= 0 {
+		if nums1[p1] > nums2[p2] {
+			nums1[p] = nums1[p1]
+			p1--
+		} else {
+			nums1[p] = nums2[p2]
+			p2--
+		}
+		p--
+	}
+
+	// If any elements remain in nums2, copy them to nums1
+	for p2 >= 0 {
+		nums1[p] = nums2[p2]
+		p2--
+		p--
+	}
+}
