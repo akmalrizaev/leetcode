f950401b1c97c4b323597d1c306b602b4dc972b6
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-05-09 08:11:24 +0500

    169. Majority Element - Array|Hash Table|D&C|Sorting|Counting

--- Easy/Array/169.Majority-Element.go
+++ Easy/Array/169.Majority-Element.go
@@ -35,3 +35,24 @@ Sorting
 Counting
 
 */
+
+package main
+
+func majorityElement(nums []int) int {
+	candidate := 0
+	count := 0
+
+	for _, num := range nums {
+		if count == 0 {
+			candidate = num
+		}
+
+		if num == candidate {
+			count++
+		} else {
+			count--
+		}
+	}
+
+	return candidate
+}
