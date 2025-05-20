07af862e865a55270de82cbea32b13d88229fc67
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-05-20 06:34:35 +0500

    303. Range Sum Query - Immutable - Array|Design|Prefix Sum

--- Easy/Array/303.Range-Sum-Query-Immutable.go
+++ Easy/Array/303.Range-Sum-Query-Immutable.go
@@ -43,3 +43,25 @@ Design
 Prefix Sum
 
 */
+
+package main
+
+type NumArray struct {
+	prefixSums []int
+}
+
+func Constructor(nums []int) NumArray {
+	n := len(nums)
+	prefixSums := make([]int, n+1)
+
+	// Compute prefix sums
+	for i := 0; i < n; i++ {
+		prefixSums[i+1] = prefixSums[i] + nums[i]
+	}
+
+	return NumArray{prefixSums: prefixSums}
+}
+
+func (this *NumArray) SumRange(left int, right int) int {
+	return this.prefixSums[right+1] - this.prefixSums[left]
+}
