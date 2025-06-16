6477bff250b7ffb1d682e998d8a673bc80c9f0f7
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-06-16 11:57:51 +0500

    628. Maximum Product of Three Numbers - Array|Math|Sorting

--- Easy/Array/628.Maximum-Product-of-Three-Numbers.go
+++ Easy/Array/628.Maximum-Product-of-Three-Numbers.go
@@ -31,3 +31,28 @@ Math
 Sorting
 
 */
+
+package main
+
+import "sort"
+
+func maximumProduct(nums []int) int {
+	// Sort the array
+	sort.Ints(nums)
+
+	// Calculate the two possible maximum products
+	n := len(nums)
+	product1 := nums[n-1] * nums[n-2] * nums[n-3] // Three largest numbers
+	product2 := nums[0] * nums[1] * nums[n-1]     // Two smallest and largest number
+
+	// Return the maximum of the two
+	return max(product1, product2)
+}
+
+// Helper function to find the maximum of two integers
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
