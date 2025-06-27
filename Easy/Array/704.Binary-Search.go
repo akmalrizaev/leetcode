01c4d399772b4ba4dde0aa1056ea1f42d89c4965
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-06-27 10:30:40 +0500

    704. Binary Search - Array|Binary Search

--- Easy/Array/704.Binary-Search.go
+++ Easy/Array/704.Binary-Search.go
@@ -32,3 +32,22 @@ Array
 Binary Search
 
 */
+
+package main
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return -1
+}
