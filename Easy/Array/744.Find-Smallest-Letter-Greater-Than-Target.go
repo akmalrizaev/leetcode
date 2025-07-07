a7cf59bd01f59f2e009df05f29e1597642c503af
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-07-07 06:55:04 +0500

    744. Find Smallest Letter Greater Than Target - Array|Binary Search

--- Easy/Array/744.Find-Smallest-Letter-Greater-Than-Target.go
+++ Easy/Array/744.Find-Smallest-Letter-Greater-Than-Target.go
@@ -38,3 +38,20 @@ Array
 Binary Search
 
 */
+
+package main
+
+func nextGreatestLetter(letters []byte, target byte) byte {
+	left, right := 0, len(letters)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+		if letters[mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+
+	return letters[left%len(letters)]
+}
