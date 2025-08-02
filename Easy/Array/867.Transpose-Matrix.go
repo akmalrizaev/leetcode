add1cbd6c85572be594c97192f9015826cd650c6
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-08-02 10:31:03 +0500

    867. Transpose Matrix - Array|Matrix|Simulation

--- Easy/Array/867.Transpose-Matrix.go
+++ Easy/Array/867.Transpose-Matrix.go
@@ -34,3 +34,21 @@ Matrix
 Simulation
 
 */
+
+package main
+
+func transpose(matrix [][]int) [][]int {
+	m, n := len(matrix), len(matrix[0])
+	result := make([][]int, n)
+	for i := range result {
+		result[i] = make([]int, m)
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			result[j][i] = matrix[i][j]
+		}
+	}
+
+	return result
+}
