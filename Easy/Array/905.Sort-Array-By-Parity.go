e6472864ed55c4f311d95a75f03f243b71fff354
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-08-14 16:40:10 +0500

    905. Sort Array By Parity - Array|Two Pointers|Sorting

--- Easy/Array/905.Sort-Array-By-Parity.go
+++ Easy/Array/905.Sort-Array-By-Parity.go
@@ -30,3 +30,27 @@ Two Pointers
 Sorting
 
 */
+
+package main
+
+import "fmt"
+
+func sortArrayByParity(nums []int) []int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		if nums[left]%2 > nums[right]%2 {
+			nums[left], nums[right] = nums[right], nums[left]
+		}
+		if nums[left]%2 == 0 {
+			left++
+		}
+		if nums[right]%2 == 1 {
+			right--
+		}
+	}
+	return nums
+}
+
+func main() {
+	fmt.Println(sortArrayByParity([]int{3, 1, 2, 4})) // [2,4,3,1] or any valid parity order
+}
