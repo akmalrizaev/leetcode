43fa55d53e2924389d9a2f196ec4a203365f527d
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-08-29 06:21:59 +0500

    942. DI String Match - Array|Two Pointers|String|Greedy

--- Easy/Array/942.DI-String-Match.go
+++ Easy/Array/942.DI-String-Match.go
@@ -36,3 +36,24 @@ String
 Greedy
 
 */
+
+package main
+
+func diStringMatch(s string) []int {
+	n := len(s)
+	res := make([]int, n+1)
+
+	low, high := 0, n
+	for i := 0; i < n; i++ {
+		if s[i] == 'I' {
+			res[i] = low
+			low++
+		} else { // 'D'
+			res[i] = high
+			high--
+		}
+	}
+	// last one will be equal (low == high)
+	res[n] = low
+	return res
+}
