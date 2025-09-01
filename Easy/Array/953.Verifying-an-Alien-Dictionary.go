94233cba5591025899cfa7f050fb343747c96410
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-09-01 11:01:43 +0500

    953. Verifying an Alien Dictionary - Array|Hash Table|String

--- Easy/Array/953.Verifying-an-Alien-Dictionary.go
+++ Easy/Array/953.Verifying-an-Alien-Dictionary.go
@@ -38,3 +38,37 @@ Hash Table
 String
 
 */
+
+package main
+
+func isAlienSorted(words []string, order string) bool {
+	// Build rank map for alien dictionary order
+	rank := make(map[byte]int)
+	for i := 0; i < len(order); i++ {
+		rank[order[i]] = i
+	}
+
+	// Compare adjacent words
+	for i := 1; i < len(words); i++ {
+		if !inCorrectOrder(words[i-1], words[i], rank) {
+			return false
+		}
+	}
+	return true
+}
+
+// Helper to compare two words
+func inCorrectOrder(w1, w2 string, rank map[byte]int) bool {
+	minLen := len(w1)
+	if len(w2) < minLen {
+		minLen = len(w2)
+	}
+
+	for i := 0; i < minLen; i++ {
+		if w1[i] != w2[i] {
+			return rank[w1[i]] < rank[w2[i]]
+		}
+	}
+	// If all prefix chars are equal, shorter word should come first
+	return len(w1) <= len(w2)
+}
