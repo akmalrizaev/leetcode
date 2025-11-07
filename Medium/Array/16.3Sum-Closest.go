93b5533087125411d25300040495479f27ef46f7
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-11-07 08:04:30 +0500

    16. 3Sum Closest - Array|Two Pointers|Sorting

--- Medium/Array/16.3Sum-Closest.go
+++ Medium/Array/16.3Sum-Closest.go
@@ -32,4 +32,59 @@ Topics
 Array
 Two Pointers
 Sorting
+
+⏱️ Complexity
+Type	Complexity
+Time	O(n²)
+Space	O(1)
+
 */
+
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+	closest := nums[0] + nums[1] + nums[2] // initialize with first 3 numbers
+
+	for i := 0; i < len(nums)-2; i++ {
+		left, right := i+1, len(nums)-1
+
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+
+			// If exact match — we can return immediately
+			if sum == target {
+				return sum
+			}
+
+			// Update closest if this sum is nearer to target
+			if math.Abs(float64(sum-target)) < math.Abs(float64(closest-target)) {
+				closest = sum
+			}
+
+			if sum < target {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+
+	return closest
+}
+
+func main() {
+	nums := []int{-1, 2, 1, -4}
+	target := 1
+	fmt.Println(threeSumClosest(nums, target)) // Output: 2
+
+	nums2 := []int{0, 0, 0}
+	target2 := 1
+	fmt.Println(threeSumClosest(nums2, target2)) // Output: 0
+}
