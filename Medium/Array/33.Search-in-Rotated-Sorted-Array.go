061105d45361c373210e8861e24f8977908d9b2e
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-11-17 17:53:19 +0500

    33. Search in Rotated Sorted Array - Array|Binary Search

--- Medium/Array/33.Search-in-Rotated-Sorted-Array.go
+++ Medium/Array/33.Search-in-Rotated-Sorted-Array.go
@@ -38,4 +38,48 @@ Topics
 Array
 Binary Search
 
+Time complexity: O(log n)
+Space: O(1)
+
 */
+
+package main
+
+import "fmt"
+
+func search(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := left + (right-left)/2
+
+		if nums[mid] == target {
+			return mid
+		}
+
+		// Left half is sorted
+		if nums[left] <= nums[mid] {
+			// Target is inside sorted left half
+			if nums[left] <= target && target < nums[mid] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		} else {
+			// Right half is sorted
+			if nums[mid] < target && target <= nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		}
+	}
+
+	return -1
+}
+
+func main() {
+	fmt.Println(search([]int{4, 5, 6, 7, 0, 1, 2}, 0)) // 4
+	fmt.Println(search([]int{4, 5, 6, 7, 0, 1, 2}, 3)) // -1
+	fmt.Println(search([]int{1}, 0))                   // -1
+}
