d2d5156facfc71108dbbe9605353882c65931966
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2025-11-19 10:34:29 +0500

    34. Find First and Last Position of Element in Sorted Array - Array|Binary Search

--- Medium/Array/34.Find-First-and-Last-Position-of-Element-in-Sorted-Array.go
+++ Medium/Array/34.Find-First-and-Last-Position-of-Element-in-Sorted-Array.go
@@ -35,4 +35,63 @@ Topics
 Array
 Binary Search
 
+⏱️ Complexity
+Type	Complexity
+Time	O(log n)
+Space	O(1)
+
 */
+
+package main
+
+import "fmt"
+
+func searchRange(nums []int, target int) []int {
+	return []int{findFirst(nums, target), findLast(nums, target)}
+}
+
+func findFirst(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	index := -1
+
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] >= target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+
+		if nums[mid] == target {
+			index = mid
+		}
+	}
+
+	return index
+}
+
+func findLast(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	index := -1
+
+	for left <= right {
+		mid := left + (right-left)/2
+		if nums[mid] <= target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+
+		if nums[mid] == target {
+			index = mid
+		}
+	}
+
+	return index
+}
+
+func main() {
+	fmt.Println(searchRange([]int{5, 7, 7, 8, 8, 10}, 8)) // [3 4]
+	fmt.Println(searchRange([]int{5, 7, 7, 8, 8, 10}, 6)) // [-1 -1]
+	fmt.Println(searchRange([]int{}, 0))                  // [-1 -1]
+}
