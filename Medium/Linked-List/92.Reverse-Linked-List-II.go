f825136ac7b35103ae7ff0dd06c9f8b795e82357
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2026-01-20 14:26:28 +0500

    92. Reverse Linked List II - Solution

--- Medium/Linked-List/92.Reverse-Linked-List-II.go
+++ Medium/Linked-List/92.Reverse-Linked-List-II.go
@@ -22,4 +22,42 @@ Could you do it in one pass?
 
 Topics
 Linked List
+
+
+⏱️ Complexity
+Type	Complexity
+Time	O(n)
+Space	O(1)
 */
+
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+	if head == nil || left == right {
+		return head
+	}
+
+	dummy := &ListNode{Next: head}
+	prev := dummy
+
+	// Step 1: Move prev to node before left
+	for i := 1; i < left; i++ {
+		prev = prev.Next
+	}
+
+	// Step 2: Reverse sublist
+	curr := prev.Next
+	for i := 0; i < right-left; i++ {
+		next := curr.Next
+		curr.Next = next.Next
+		next.Next = prev.Next
+		prev.Next = next
+	}
+
+	return dummy.Next
+}
