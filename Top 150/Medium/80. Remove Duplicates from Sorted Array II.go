18a6dd9f179b8f48d1c83a3bf9f3055033efd40d
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2026-01-28 18:35:49 +0500

    80. Remove Duplicates from Sorted Array II - Solution

--- Top 150/Medium/80. Remove Duplicates from Sorted Array II.go
+++ Top 150/Medium/80. Remove Duplicates from Sorted Array II.go
@@ -48,5 +48,28 @@ Constraints:
 nums is sorted in non-decreasing order.
 
 
+⏱️ Complexity
+Type	Complexity
+Time	O(n)
+Space	O(1)
 
 */
+
+package main
+
+func removeDuplicates(nums []int) int {
+	if len(nums) <= 2 {
+		return len(nums)
+	}
+
+	i := 2 // First two are always valid
+
+	for j := 2; j < len(nums); j++ {
+		if nums[j] != nums[i-2] {
+			nums[i] = nums[j]
+			i++
+		}
+	}
+
+	return i
+}
