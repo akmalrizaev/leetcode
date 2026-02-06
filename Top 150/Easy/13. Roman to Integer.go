09e59fd5bd73ac0977312b98d0878e72de4f0cda
Akmal Rizaev <akmaljon.rizayev@gmail.com>
2026-02-06 18:09:56 +0500

    13. Roman to Integer - Hash Table|Math|String

--- Top 150/Easy/13. Roman to Integer.go
+++ Top 150/Easy/13. Roman to Integer.go
@@ -53,4 +53,40 @@ String
 Hint 1
 Problem is simpler to solve by working the string from back to front and using a map.
 
+
 */
+
+/*
+
+⏱️ Complexity
+Type	Complexity
+Time	O(n)
+Space	O(1)
+
+*/
+
+package main
+
+func romanToInt(s string) int {
+	values := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	result := 0
+
+	for i := 0; i < len(s); i++ {
+		if i+1 < len(s) && values[s[i]] < values[s[i+1]] {
+			result -= values[s[i]]
+		} else {
+			result += values[s[i]]
+		}
+	}
+
+	return result
+}
